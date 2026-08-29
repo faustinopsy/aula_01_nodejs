8827b36dbbb4bfedb7af260db3036950e0a91c0e
faustinopsy <rodrigohipnose@gmail.com>
2026-08-29 09:51:17 -0300

    criado o primeiro servidor com o express

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # aula_01_nodejs
 aula 00 de node - backend em js
+
+# terminal bloqueand
+
+```
+Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process
+```
+
+# instalar bibliotecas
+```
+npm install express
+npm install --save-dev nodemon
+npm install dotenv
+```
+
+# comando básicos
+- git add .
+- git commit -m "mensagem"
+- git push
--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("Olá mundo!")
+import express from 'express'
+
+const app = express()
+
+// função callback
+app.get('/busca', (req, res) => {
+    console.log(req)
+  res.send('olá mundo')
+})
+
+app.get('/busca/:id', (req, res) => {
+    const numero = req.params.id
+    console.log(numero)
+    const mensagem = ` O número requisitado é: ${numero} `
+  res.send(mensagem)
+})
+
+app.listen(3000, () => {
+  console.log('Servidor rodando em http://127.0.0.1:3000')
+})
